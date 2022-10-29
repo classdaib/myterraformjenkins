cda0a1b23bb87ee0dfe80dc4353b56929598f6e9
DAIB OMOTAYO <daibomotayo@DAIBs-MBP.attlocal.net>
2022-10-28 19:42:22 -0500

    created one additional instance

--- main.tf
+++ main.tf
@@ -77,3 +77,12 @@ resource "aws_instance" "web_7" {
     Name = "Isaac"
   }
 }
+
+resource "aws_instance" "web_8" {
+  ami           = "ami-09d3b3274b6c5d4aa"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "Ade"
+  }
+}
