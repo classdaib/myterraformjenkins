a415117076291901361dc68daeec87d844b7dd09
DAIB OMOTAYO <daibomotayo@DAIBs-MBP.attlocal.net>
2022-10-28 19:22:52 -0500

    added three addditional servers

--- main.tf
+++ main.tf
@@ -6,3 +6,33 @@ resource "aws_instance" "web" {
     Name = "jekins-pipetutoria"
   }
 }
+
+
+resource "aws_instance" "web_1" {
+  ami           = "ami-09d3b3274b6c5d4aa"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "Carita"
+  }
+}
+
+
+resource "aws_instance" "web_2" {
+  ami           = "ami-09d3b3274b6c5d4aa"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "Olawale"
+  }
+}
+
+
+resource "aws_instance" "web+3" {
+  ami           = "ami-09d3b3274b6c5d4aa"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "Otunba"
+  }
+}
